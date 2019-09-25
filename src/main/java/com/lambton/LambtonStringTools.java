dcbad69cfb3ce9eb6c496d33783d4bba87dda445
macstudent <dandiwal357@gmail.com>
2019-09-25 18:39:43 -0400

    reverse function

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -2,8 +2,18 @@ package com.lambton;
 
 public class LambtonStringTools
 {
-public void reverse (String h)
+public String reverse (String h)
 {
+   int t1 = 0;
+   char[] charString = h.toCharArray();
+   char[] revString=new char[h.length()];
+   for (t1=charString.length-1;t1>=0;t1--)
+   {
+       revString[t1++]=charString[t1];
 
+   }
+   String lastReverse;
+    lastReverse =new String(revString);
+    return lastReverse;
 }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,5 @@
+import com.lambton.LambtonStringTools;
+
 public class main
 {
     public static void main(String[] args) {
