098f109a774c61d4e477af8aa921897f56442eab
macstudent <dandiwal357@gmail.com>
2019-09-25 19:33:19 -0400

    binary to decimal

--- src/main/java/mainfile.java
+++ src/main/java/mainfile.java
@@ -5,7 +5,7 @@ public class mainfile
     public static void main (String[] args) {
         LambtonStringTools t1 = new LambtonStringTools();
         System.out.println(t1.reverse("lambton"));
-        System.out.println(t1.binaryToDecimal("101101"));
+        System.out.println(t1.binaryToDecimal("10100011101101"));
 
 
     }
